2c12de869317267b646b2c84c446eb0e7ba32444
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-03-11 21:54:06 -0600

    second tests

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -4,7 +4,8 @@ export const Form = () => {
     <>
       <h1>created product</h1>
       <form>
-
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" value="" />
       </form>
     </>
   )
--- src/form/form.test.js
+++ src/form/form.test.js
@@ -1,11 +1,16 @@
-import {Form} from './Form'
+import { Form } from './Form';
 import { render } from '@testing-library/react';
 
 describe('Form', () => {
-  describe('when the form is mounted', () => {
-    it('there must be a crate product form page', () => {
-      const {queryByText} = render(<Form />)
-      expect(queryByText(/created product/i)).toBeTruthy()
+	describe('when the form is mounted', () => {
+		it('there must be a crate product form page', () => {
+			const { getByRole } = render(<Form />);
+			expect(getByRole('heading', { name: /created product/i })).toBeTruthy();
+		});
+
+    it('should exists the fields: name, size, type (electronic, furniture, clothing)', () => {
+      const { getByLabelText } = render(<Form />);
+      expect(getByLabelText(/name/i)).toBeTruthy();
     })
-  })
-})
+	});
+});
