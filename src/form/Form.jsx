fac1b85a3000a70c50eeb0b1328ebf65e6d057be
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-08 20:49:41 -0500

    add tests for name and size

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,1 +1,12 @@
-export const Form = () => <div>create product</div>
+import { TextField } from "@material-ui/core"
+
+export const Form = () => {
+
+  return (
+    <>
+      <h1>create product</h1>
+      <TextField id="name" label="name" />
+      <TextField id="size" label="size" />
+    </>
+  )
+}
--- src/form/form.test.js
+++ src/form/form.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Form } from './Form';
 
 describe('when the form is mounted', () => {
 	it('render must be a create product form page', () => {
 		const { queryByText } = render(<Form />);
-
 		expect(queryByText(/create product/)).not.toBeNull();
+	});
+	it('should exist the fields: name, size, type (electronic, furniture, clothing)', () => {
+		const { getByLabelText } = render(<Form />);
+		expect(getByLabelText(/name/)).not.toBeNull();
+		expect(getByLabelText(/size/)).not.toBeNull();
 	});
 });
