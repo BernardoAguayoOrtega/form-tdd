4edc6f19fd415f078631ff475dc91e4db0d56f2d
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-08 20:59:23 -0500

    add tests fir diferent kinds of inputs and its options

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,12 +1,28 @@
-import { TextField } from "@material-ui/core"
+import { InputLabel, Select, TextField } from "@material-ui/core"
 
 export const Form = () => {
 
   return (
     <>
       <h1>create product</h1>
-      <TextField id="name" label="name" />
-      <TextField id="size" label="size" />
+      <form>
+        <TextField label="name" id="name" />
+        <TextField label="size" id="size" />
+        <InputLabel htmlFor="type">Type</InputLabel>
+        <Select
+          native
+          value=''
+          inputProps={{
+            name: 'type',
+            id: 'type',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value='electronic'>electronic</option>
+          <option value='furniture'>furniture</option>
+          <option value='clothing'>clothing</option>
+        </Select>
+      </form>
     </>
   )
 }
--- src/form/form.test.js
+++ src/form/form.test.js
@@ -5,11 +5,16 @@ import { Form } from './Form';
 describe('when the form is mounted', () => {
 	it('render must be a create product form page', () => {
 		const { queryByText } = render(<Form />);
-		expect(queryByText(/create product/)).not.toBeNull();
+		expect(queryByText(/create product/i)).not.toBeNull();
 	});
 	it('should exist the fields: name, size, type (electronic, furniture, clothing)', () => {
-		const { getByLabelText } = render(<Form />);
-		expect(getByLabelText(/name/)).not.toBeNull();
-		expect(getByLabelText(/size/)).not.toBeNull();
+		const { getByLabelText, queryByText } = render(<Form />);
+		expect(getByLabelText(/name/i)).not.toBeNull();
+		expect(getByLabelText(/size/i)).not.toBeNull();
+		expect(getByLabelText(/type/i)).not.toBeNull();
+
+		expect(queryByText(/electronic/i)).not.toBeNull();
+		expect(queryByText(/furniture/i)).not.toBeNull();
+		expect(queryByText(/clothing/i)).not.toBeNull();
 	});
 });
